6b6dd09555ea8cc9e6b4f1630a5e8815cb9703c9
Diogo Silva <diogomfsilva98@ua.pt>
2023-06-01 16:42:01 +0100

    test

--- mail-mover/backend/mailMoverPlatform/src/test/java/tqs/project/mailMoverPlatform/integrationTests/AcpControllerIT.java
+++ mail-mover/backend/mailMoverPlatform/src/test/java/tqs/project/mailMoverPlatform/integrationTests/AcpControllerIT.java
@@ -1,6 +1,6 @@
 package tqs.project.mailMoverPlatform.integrationTests;
 
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
