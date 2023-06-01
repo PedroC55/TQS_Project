35e0416250a4f8ecfafbcdde47884e3ecda6097c
Manuel Diaz <manu.guerra.diaz@gmail.com>
2023-06-01 17:22:18 +0100

    public_tests_update

--- mail-mover/backend/mailMoverPlatform/src/test/java/tqs/project/mailMoverPlatform/controllerTests/AcpControllerMockMvcTest.java
+++ mail-mover/backend/mailMoverPlatform/src/test/java/tqs/project/mailMoverPlatform/controllerTests/AcpControllerMockMvcTest.java
@@ -1,7 +1,6 @@
 package tqs.project.mailMoverPlatform.controllerTests;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
